aa03b96fe68fd40d909a173b27fc3a26dcbe7ab1
忆兮 <26674891@qq.com>
2016-07-07 10:00:49 +0800

    init项目

---
+++ ReGit/ReGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReGit
+//
+//  Created by lanou3g on 16/7/7.
+//  Copyright © 2016年 wxerters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
